e0425de219f8f3d0df38286331bdc2b0c528249f
Lee-jr-81 <129674277+Lee-jr-81@users.noreply.github.com>
2024-02-09 18:53:13 +0000

    Added different text colours and changed font weights

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      customFont: ['"Work sans"', "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
