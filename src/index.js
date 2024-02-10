172e17c1f6ee33cc06f2abedd1396acbd65b2210
Lee-jr-81 <129674277+Lee-jr-81@users.noreply.github.com>
2024-02-10 16:38:29 +0000

    Added the js logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 "use strict";
 
-// selecting elements
+// Select the elements
+const first = document.getElementById("para1");
+const second = document.getElementById("para2");
+const third = document.getElementById("para3");
+const fourth = document.getElementById("para4");
+
+// Add the event
+document.querySelectorAll("button").forEach((button, index) => {
+  button.onclick = (event) => {
+    switch (index) {
+      case 0:
+        first.classList.toggle("hidden");
+        break;
+      case 1:
+        second.classList.toggle("hidden");
+        break;
+      case 2:
+        third.classList.toggle("hidden");
+        break;
+      case 3:
+        fourth.classList.toggle("hidden");
+        break;
+    }
+  };
+});
